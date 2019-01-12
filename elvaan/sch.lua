738ade53c604947074fd7a4c1d8b9114bacf3284
yyoshisaur <halkload@gmail.com>
2019-01-13 01:37:10 +0900

    八輪の帯と属性帯を混在できるように修正

--- elvaan/sch.lua
+++ elvaan/sch.lua
@@ -6,6 +6,54 @@ function get_sets()
     sets.midcast = {}
     sets.aftercast = {}
     
+    weather = {}
+    weather_strong = {}
+    weaken_element = {}
+    elemental_obi = {}
+
+    -- 弱天候
+    weather['火'] = '熱波'
+    weather['水'] = '雨'
+    weather['雷'] = '雷'
+    weather['土'] = '砂塵'
+    weather['風'] = '風'
+    weather['氷'] = '雪'
+    weather['光'] = 'オーロラ'
+    weather['闇'] = '妖霧'
+
+    -- 強天候
+    weather_strong['火'] = '灼熱波'
+    weather_strong['水'] = 'スコール'
+    weather_strong['雷'] = '雷雨'
+    weather_strong['土'] = '砂嵐'
+    weather_strong['風'] = '暴風'
+    weather_strong['氷'] = '吹雪'
+    weather_strong['光'] = '神光'
+    weather_strong['闇'] = '闇'
+
+    -- 弱点属性
+    weaken_element['火'] = '水'
+    weaken_element['水'] = '雷'
+    weaken_element['雷'] = '土'
+    weaken_element['土'] = '風'
+    weaken_element['風'] = '氷'
+    weaken_element['氷'] = '火'
+    weaken_element['光'] = '闇'
+    weaken_element['闇'] = '光'
+
+    -- 所持している属性帯(所持していない場合はnil)
+    elemental_obi = {}
+    elemental_obi['火'] = nil
+    elemental_obi['水'] = nil
+    elemental_obi['雷'] = nil
+    elemental_obi['土'] = nil
+    elemental_obi['風'] = nil
+    elemental_obi['氷'] = nil
+    elemental_obi['光'] = {waist="光輪の帯",}
+    elemental_obi['闇'] = nil
+
+    hachirin = {waist="八輪の帯",}
+
     is_cp = false
     is_immanence = false
 
@@ -272,7 +320,7 @@ function midcast(spell)
             if sets.helix:contains(spell.name) then
                 set_equip = sets.midcast.helix_mb
             else
-                set_equip = set_combine(sets.midcast.magic_mb, get_elemental_obi(spell.element))
+                set_equip = set_combine(sets.midcast.magic_mb, get_hachirin(spell.element))
             end
         end
     end
@@ -338,5 +386,32 @@ function get_elemental_obi(spell_element)
         return sets.obi
     else
         return nil
+    end
+end
+
+function get_hachirin(spell_element)
+    if elemental_obi[spell_element] then
+        if spell_element == world.weather_element or spell_element == world.day_element then
+            return elemental_obi[spell_element]
+        else
+            return nil
+        end
+    else
+        if weather_strong[spell_element] == world.weather then
+            windower.add_to_chat(122,'strong')
+            return hachirin
+        elseif weather[spell_element] == world.weather
+               and world.day_element ~= weaken_element[spell_element] then
+            windower.add_to_chat(122,'week')
+            return hachirin
+        elseif world.day_element == spell_element
+               and world.weather ~= weather_strong[weaken_element[spell_element]]
+               and world.weather ~= weather[weaken_element[spell_element]] then
+            windower.add_to_chat(122,'day')
+            return hachirin
+        else
+            windower.add_to_chat(122,'else')
+            return nil
+        end
     end
 end
