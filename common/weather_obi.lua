d6a7cc0393e789f3d622738b786eb56ce4f2da29
yyoshisaur <halkload@gmail.com>
2019-03-13 00:25:38 +0900

    属性帯の処理が別ファイルに移動

---
+++ common/weather_obi.lua
@@ -0,0 +1,105 @@
+local weather = {}
+local weather_strong = {}
+local weaken_element = {}
+local elemental_obi = {}
+
+-- 弱天候
+weather['火'] = '熱波'
+weather['水'] = '雨'
+weather['雷'] = '雷'
+weather['土'] = '砂塵'
+weather['風'] = '風'
+weather['氷'] = '雪'
+weather['光'] = 'オーロラ'
+weather['闇'] = '妖霧'
+
+-- 強天候
+weather_strong['火'] = '灼熱波'
+weather_strong['水'] = 'スコール'
+weather_strong['雷'] = '雷雨'
+weather_strong['土'] = '砂嵐'
+weather_strong['風'] = '暴風'
+weather_strong['氷'] = '吹雪'
+weather_strong['光'] = '神光'
+weather_strong['闇'] = '闇'
+
+-- 弱点属性
+weaken_element['火'] = '水'
+weaken_element['水'] = '雷'
+weaken_element['雷'] = '土'
+weaken_element['土'] = '風'
+weaken_element['風'] = '氷'
+weaken_element['氷'] = '火'
+weaken_element['光'] = '闇'
+weaken_element['闇'] = '光'
+
+-- 所持している属性帯(所持していない場合はnil)
+elemental_obi['火'] = nil
+elemental_obi['水'] = nil
+elemental_obi['雷'] = nil
+elemental_obi['土'] = nil
+elemental_obi['風'] = nil
+elemental_obi['氷'] = nil
+elemental_obi['光'] = nil
+elemental_obi['闇'] = nil
+
+local hachirin = {waist="八輪の帯",}
+
+function get_hachirin(spell_element)
+    if elemental_obi[spell_element] then
+        if spell_element == world.weather_element or spell_element == world.day_element then
+            return elemental_obi[spell_element]
+        else
+            return nil
+        end
+    else
+        if weather_strong[spell_element] == world.weather then
+            return hachirin
+        elseif weather[spell_element] == world.weather
+                and world.day_element ~= weaken_element[spell_element] then
+            return hachirin
+        elseif world.day_element == spell_element
+                and world.weather ~= weather_strong[weaken_element[spell_element]]
+                and world.weather ~= weather[weaken_element[spell_element]] then
+            return hachirin
+        else
+            return nil
+        end
+    end
+end
+
+function init_elemental_obi()
+    elemental_obi = {}
+    elemental_obi['火'] = nil
+    elemental_obi['水'] = nil
+    elemental_obi['雷'] = nil
+    elemental_obi['土'] = nil
+    elemental_obi['風'] = nil
+    elemental_obi['氷'] = nil
+    elemental_obi['光'] = nil
+    elemental_obi['闇'] = nil
+end
+
+function set_elemental_obi(obi)
+    if obi == '火輪の帯' then
+        elemental_obi['火'] = {waist=obi,}
+    elseif obi == '水輪の帯' then
+        elemental_obi['水'] = {waist=obi,}
+    elseif obi == '雷輪の帯' then
+        elemental_obi['雷'] = {waist=obi,}
+    elseif obi == '土輪の帯' then
+        elemental_obi['土'] = {waist=obi,}
+    elseif obi == '風輪の帯' then
+        elemental_obi['風'] = {waist=obi,}
+    elseif obi == '氷輪の帯' then
+        elemental_obi['氷'] = {waist=obi,}
+    elseif obi == '光輪の帯' then
+        elemental_obi['光'] = {waist=obi,}
+    elseif obi == '闇輪の帯' then
+        elemental_obi['闇'] = {waist=obi,}
+    end
+end
+
+function get_elemental_obi()
+    return elemental_obi
+end
--- elvaan/sch.lua
+++ elvaan/sch.lua
@@ -1,3 +1,4 @@
+include('weather_obi')
 function get_sets()
     set_language('japanese')
     
@@ -6,53 +7,55 @@ function get_sets()
     sets.midcast = {}
     sets.aftercast = {}
     
-    weather = {}
-    weather_strong = {}
-    weaken_element = {}
-    elemental_obi = {}
+    -- weather = {}
+    -- weather_strong = {}
+    -- weaken_element = {}
+    -- elemental_obi = {}
 
-    -- 弱天候
-    weather['火'] = '熱波'
-    weather['水'] = '雨'
-    weather['雷'] = '雷'
-    weather['土'] = '砂塵'
-    weather['風'] = '風'
-    weather['氷'] = '雪'
-    weather['光'] = 'オーロラ'
-    weather['闇'] = '妖霧'
+    -- -- 弱天候
+    -- weather['火'] = '熱波'
+    -- weather['水'] = '雨'
+    -- weather['雷'] = '雷'
+    -- weather['土'] = '砂塵'
+    -- weather['風'] = '風'
+    -- weather['氷'] = '雪'
+    -- weather['光'] = 'オーロラ'
+    -- weather['闇'] = '妖霧'
 
-    -- 強天候
-    weather_strong['火'] = '灼熱波'
-    weather_strong['水'] = 'スコール'
-    weather_strong['雷'] = '雷雨'
-    weather_strong['土'] = '砂嵐'
-    weather_strong['風'] = '暴風'
-    weather_strong['氷'] = '吹雪'
-    weather_strong['光'] = '神光'
-    weather_strong['闇'] = '闇'
+    -- -- 強天候
+    -- weather_strong['火'] = '灼熱波'
+    -- weather_strong['水'] = 'スコール'
+    -- weather_strong['雷'] = '雷雨'
+    -- weather_strong['土'] = '砂嵐'
+    -- weather_strong['風'] = '暴風'
+    -- weather_strong['氷'] = '吹雪'
+    -- weather_strong['光'] = '神光'
+    -- weather_strong['闇'] = '闇'
 
-    -- 弱点属性
-    weaken_element['火'] = '水'
-    weaken_element['水'] = '雷'
-    weaken_element['雷'] = '土'
-    weaken_element['土'] = '風'
-    weaken_element['風'] = '氷'
-    weaken_element['氷'] = '火'
-    weaken_element['光'] = '闇'
-    weaken_element['闇'] = '光'
+    -- -- 弱点属性
+    -- weaken_element['火'] = '水'
+    -- weaken_element['水'] = '雷'
+    -- weaken_element['雷'] = '土'
+    -- weaken_element['土'] = '風'
+    -- weaken_element['風'] = '氷'
+    -- weaken_element['氷'] = '火'
+    -- weaken_element['光'] = '闇'
+    -- weaken_element['闇'] = '光'
 
     -- 所持している属性帯(所持していない場合はnil)
-    elemental_obi = {}
-    elemental_obi['火'] = nil
-    elemental_obi['水'] = nil
-    elemental_obi['雷'] = nil
-    elemental_obi['土'] = nil
-    elemental_obi['風'] = nil
-    elemental_obi['氷'] = nil
-    elemental_obi['光'] = {waist="光輪の帯",}
-    elemental_obi['闇'] = nil
+    -- elemental_obi = {}
+    -- elemental_obi['火'] = nil
+    -- elemental_obi['水'] = nil
+    -- elemental_obi['雷'] = nil
+    -- elemental_obi['土'] = nil
+    -- elemental_obi['風'] = nil
+    -- elemental_obi['氷'] = nil
+    -- elemental_obi['光'] = {waist="光輪の帯",}
+    -- elemental_obi['闇'] = nil
 
-    hachirin = {waist="八輪の帯",}
+    -- hachirin = {waist="八輪の帯",}
+    init_elemental_obi()
+    set_elemental_obi("光輪の帯")
 
     is_cp = false
     is_immanence = false
@@ -62,7 +65,7 @@ function get_sets()
     sets.helix = T{'火門の計', '氷門の計', '風門の計', '土門の計', '雷門の計', '水門の計', '光門の計', '闇門の計', '火門の計II', '氷門の計II', '風門の計II', '土門の計II', '雷門の計II', '水門の計II', '光門の計II', '闇門の計II'}
 
     sets.cp = {back="アピトマント+1"}
-    sets.obi = {waist="八輪の帯"}
+    -- sets.obi = {waist="八輪の帯"}
     
     sets.precast.fc = {
         ammo="インカントストーン",
@@ -380,33 +383,33 @@ function self_command(command)
     end
 end
 
-function get_elemental_obi(spell_element)
-    if spell_element == world.weather_element or spell_element == world.day_element then
-        return sets.obi
-    else
-        return nil
-    end
-end
+-- function get_elemental_obi(spell_element)
+--     if spell_element == world.weather_element or spell_element == world.day_element then
+--         return sets.obi
+--     else
+--         return nil
+--     end
+-- end
 
-function get_hachirin(spell_element)
-    if elemental_obi[spell_element] then
-        if spell_element == world.weather_element or spell_element == world.day_element then
-            return elemental_obi[spell_element]
-        else
-            return nil
-        end
-    else
-        if weather_strong[spell_element] == world.weather then
-            return hachirin
-        elseif weather[spell_element] == world.weather
-               and world.day_element ~= weaken_element[spell_element] then
-            return hachirin
-        elseif world.day_element == spell_element
-               and world.weather ~= weather_strong[weaken_element[spell_element]]
-               and world.weather ~= weather[weaken_element[spell_element]] then
-            return hachirin
-        else
-            return nil
-        end
-    end
-end
+-- function get_hachirin(spell_element)
+--     if elemental_obi[spell_element] then
+--         if spell_element == world.weather_element or spell_element == world.day_element then
+--             return elemental_obi[spell_element]
+--         else
+--             return nil
+--         end
+--     else
+--         if weather_strong[spell_element] == world.weather then
+--             return hachirin
+--         elseif weather[spell_element] == world.weather
+--                and world.day_element ~= weaken_element[spell_element] then
+--             return hachirin
+--         elseif world.day_element == spell_element
+--                and world.weather ~= weather_strong[weaken_element[spell_element]]
+--                and world.weather ~= weather[weaken_element[spell_element]] then
+--             return hachirin
+--         else
+--             return nil
+--         end
+--     end
+-- end
