a5ba5dbaefa2c56eff74dd00ee42327a7b55f0fb
yyoshisaur <halkload@gmail.com>
2019-06-10 13:21:12 +0900

    黒魔道士を追加

---
+++ elvaan/blm.lua
@@ -0,0 +1,167 @@
+function get_sets()
+    set_language('japanese')
+    
+    sets.precast = {}
+    sets.precast.ws = {}
+    sets.midcast = {}
+    sets.aftercast = {}
+
+    sets.precast.fc = {
+        ammo="ペムフレドタスラム",
+        head={ name="マーリンフード", augments={'"Fast Cast"+7','INT+8','Mag. Acc.+15',}},
+        body={ name="テルキネシャジュブ", augments={'Mag. Evasion+23','"Fast Cast"+5','Enh. Mag. eff. dur. +10',}},
+        hands={ name="テルキネグローブ", augments={'Mag. Evasion+23','"Fast Cast"+5','Enh. Mag. eff. dur. +10',}},
+        legs={ name="サイクロスラッパ", augments={'MP+80','Mag. Acc.+15','"Fast Cast"+7',}},
+        feet={ name="マーリンクラッコー", augments={'Mag. Acc.+10 "Mag.Atk.Bns."+10','"Fast Cast"+7',}},
+        neck="ボルトサージトルク",
+        waist="チャネラーストーン",
+        left_ear="エテオレートピアス",
+        right_ear="ロケイシャスピアス",
+        left_ring="キシャールリング",
+        right_ring="プロリクスリング",
+    }
+
+    sets.precast.fc_elemental = {
+        head={ name="マーリンフード", augments={'"Fast Cast"+7','INT+8','Mag. Acc.+15',}},
+        body="マルクィサイオ+2",
+        legs="マルクィトルーズ+2",
+        feet={ name="マーリンクラッコー", augments={'Mag. Acc.+10 "Mag.Atk.Bns."+10','"Fast Cast"+7',}},
+        left_ear="バーカロルピアス",
+    }
+
+    sets.precast.ws.mp = {
+        ammo="プシロメン",
+        head={ name="ヴァニヤフード", augments={'MP+50','"Fast Cast"+10','Haste+2%',}},
+        body={ name="ウェーザーローブ+1", augments={'MP+120',}},
+        hands={ name="オトミグローブ", augments={'HP+30','MP+30','MP+30',}},
+        legs={ name="サイクロスラッパ", augments={'MP+80','Mag. Acc.+15','"Fast Cast"+7',}},
+        feet={ name="サイクロスブーツ", augments={'MP+50','INT+7','"Conserve MP"+6',}},
+        neck="デュアルカラー+1",
+        waist="神術帯+1",
+        left_ear="エテオレートピアス",
+        right_ear="アンドアーピアス",
+        left_ring="メフィタスリング+1",
+        right_ring="メフィタスリング",
+        back="フィフォレケープ+1",
+    }
+
+    sets.precast.ws['ミルキル'] = sets.precast.ws.mp
+
+    sets.midcast.elemental = {
+        ammo="ペムフレドタスラム",
+        head={ name="マーリンフード", augments={'Mag. Acc.+23 "Mag.Atk.Bns."+23','INT+7','Mag. Acc.+8','"Mag.Atk.Bns."+11',}},
+        body="ＳＰコート+1",
+        hands={ name="ＡＭゲージ+1", augments={'INT+12','Mag. Acc.+20','"Mag.Atk.Bns."+20',}},
+        legs={ name="マーリンシャルワ", augments={'Mag. Acc.+23 "Mag.Atk.Bns."+23','INT+5','"Mag.Atk.Bns."+14',}},
+        feet="ジャリピガッシュ+2",
+        neck="水影の首飾り",
+        waist="エスカンストーン",
+        left_ear="バーカロルピアス",
+        right_ear="怯懦の耳",
+        left_ring="女王の指輪+1",
+        right_ring="女王の指輪+1",
+        back="アピトマント+1",
+    }
+
+    sets.midcast.enhance_duration = {
+        head={ name="テルキネキャップ", augments={'Mag. Evasion+25','"Fast Cast"+5','Enh. Mag. eff. dur. +10',}},
+        body={ name="テルキネシャジュブ", augments={'Mag. Evasion+23','"Fast Cast"+5','Enh. Mag. eff. dur. +10',}},
+        hands={ name="テルキネグローブ", augments={'Mag. Evasion+23','"Fast Cast"+5','Enh. Mag. eff. dur. +10',}},
+        legs={ name="テルキネブラコーニ", augments={'Mag. Evasion+24','"Fast Cast"+5','Enh. Mag. eff. dur. +10',}},
+        feet={ name="テルキネピガッシュ", augments={'Mag. Evasion+23','"Fast Cast"+5','Enh. Mag. eff. dur. +10',}},
+        waist="ニヌルタサッシュ",
+    }
+
+    sets.aftercast.idle = {
+        ammo="プシロメン",
+        head={ name="テルキネキャップ", augments={'Mag. Evasion+25','"Fast Cast"+5','Enh. Mag. eff. dur. +10',}},
+        body="マルクィサイオ+2",
+        hands={ name="テルキネグローブ", augments={'Mag. Evasion+23','"Fast Cast"+5','Enh. Mag. eff. dur. +10',}},
+        legs={ name="テルキネブラコーニ", augments={'Mag. Evasion+24','"Fast Cast"+5','Enh. Mag. eff. dur. +10',}},
+        feet={ name="テルキネピガッシュ", augments={'Mag. Evasion+23','"Fast Cast"+5','Enh. Mag. eff. dur. +10',}},
+        neck="ロリケートトルク+1",
+        waist="スリポーサッシュ",
+        left_ear="エテオレートピアス",
+        right_ear="驕慢の耳",
+        left_ring="守りの指輪",
+        right_ring="シュネデックリング",
+        back="アピトマント+1",
+    }
+
+    -- マクロのブック, セット変更
+    send_command('input /macro book 7; wait 0.5; input /macro set 1')
+end
+
+function precast(spell)
+    local set_equip = nil
+
+    if spell.type == 'WhiteMagic' then
+        set_equip = sets.precast.fc
+    elseif spell.type == 'BlackMagic' then
+        if spell.skill == '精霊魔法' then
+            set_equip = sets.precast.fc_elemental
+        else
+            set_equip = sets.precast.fc
+        end
+    elseif spell.type == 'WeaponSkill' then
+        if sets.precast.ws[spell.name] then
+            set_equip = sets.precast.ws[spell.name]
+        end
+    elseif spell.type == 'Trust' then
+        set_equip = sets.precast.fc
+    end
+    
+    if set_equip then
+        equip(set_equip)
+    end
+end
+
+function midcast(spell)
+    local set_equip = nil
+    
+    if string.find(spell.name, 'ケアル') then
+        -- set_equip = sets.midcast.cure
+    elseif spell.skill == '強化魔法' then
+        if spell.name == 'ストンスキン' then
+            set_equip = set_combine(sets.midcast.enhance_duration, {neck='ノデンズゴルゲット', left_ear='アースクライピアス', waist="ジーゲルサッシュ",})
+        else
+            set_equip = sets.midcast.enhance_duration
+        end
+    elseif spell.skill == '弱体魔法' or spell.skill == '暗黒魔法' then
+        set_equip = sets.midcast.elementa
+    elseif spell.skill == '精霊魔法' then
+        set_equip = sets.midcast.elemental
+    end
+    
+    if set_equip then
+        equip(set_equip)
+    end
+end
+
+function aftercast(spell)
+    local set_equip = nil
+    
+    if player.status == 'Engaged' then
+        set_equip = sets.aftercast.melee
+    else
+        set_equip = sets.aftercast.idle
+    end
+    
+    if set_equip then
+        equip(set_equip)
+    end
+end
+
+function status_change(new, old)
+    local set_equip = nil
+    
+    if new == 'Idle' then
+        set_equip = sets.aftercast.idle
+    elseif new == 'Engaged' then
+        set_equip = sets.aftercast.melee
+    end
+    
+    if set_equip then
+        equip(set_equip)
+    end
+end
